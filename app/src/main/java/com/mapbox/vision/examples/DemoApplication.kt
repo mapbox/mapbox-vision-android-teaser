acea21e4c93f5a239c42c4218db8034eefd255bb
Boris Denisenko <Shumer512@gmail.com>
2018-10-17 16:03:46 +0300

    Add Crashlytics

--- app/src/main/java/com/mapbox/vision/examples/DemoApplication.kt
+++ app/src/main/java/com/mapbox/vision/examples/DemoApplication.kt
@@ -3,11 +3,16 @@ package com.mapbox.vision.examples
 import android.support.multidex.MultiDexApplication
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.vision.VisionManager
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class DemoApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         VisionManager.init(this, getString(R.string.mapbox_access_token))
     }
