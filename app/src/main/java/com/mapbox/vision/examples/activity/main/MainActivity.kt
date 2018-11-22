868d3c86aaae77996d6d3d2da078a3e9843bedb2
Boris Denisenko <Shumer512@gmail.com>
2018-11-22 13:29:37 +0300

    Fixes after merge

--- app/src/main/java/com/mapbox/vision/examples/activity/main/MainActivity.kt
+++ app/src/main/java/com/mapbox/vision/examples/activity/main/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.text.Html
+import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.ImageView
