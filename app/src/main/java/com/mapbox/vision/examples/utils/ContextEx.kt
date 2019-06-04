fb667c445550d657e747d545f5c903c52bd97039
Dmitry Yunitsky <dmitry.yunitsky@gmail.com>
2019-06-04 16:50:09 +0300

    Add assets for UK. Swap used assets when country changes.

--- app/src/main/java/com/mapbox/vision/examples/utils/ContextEx.kt
+++ app/src/main/java/com/mapbox/vision/examples/utils/ContextEx.kt
@@ -5,3 +5,12 @@ import android.content.Context
 fun Context.dpToPx(dp: Float) = dp * this.resources.displayMetrics.density
 
 fun Context.pxToDp(px: Float) = px / this.resources.displayMetrics.density
+
+enum class ResourceType(val typeName: kotlin.String) {
+    Drawable("drawable"),
+    String("string")
+}
+
+fun Context.getResId(name: String, type : ResourceType) = resources.getIdentifier(
+    name, type.typeName, packageName
+)
