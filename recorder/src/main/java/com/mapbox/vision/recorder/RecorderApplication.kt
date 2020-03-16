62ba701e917dffbe57b81964e34ee6f638051def
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2020-03-16 16:16:33 +0300

    Add crashlytics to debug builds as well

--- recorder/src/main/java/com/mapbox/vision/recorder/RecorderApplication.kt
+++ recorder/src/main/java/com/mapbox/vision/recorder/RecorderApplication.kt
@@ -1,17 +1,12 @@
 package com.mapbox.vision.recorder
 
 import androidx.multidex.MultiDexApplication
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.mapbox.vision.VisionManager
 
 class RecorderApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        FirebaseApp.initializeApp(this)?.let {
-            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
-        }
         VisionManager.init(this, getString(R.string.mapbox_access_token))
     }
 }
--- replayer/src/main/java/com/mapbox/vision/replayer/ReplayerApplication.kt
+++ replayer/src/main/java/com/mapbox/vision/replayer/ReplayerApplication.kt
@@ -1,8 +1,6 @@
 package com.mapbox.vision.replayer
 
 import androidx.multidex.MultiDexApplication
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.vision.VisionManager
 
@@ -10,9 +8,6 @@ class ReplayerApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        FirebaseApp.initializeApp(this)?.let {
-            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
-        }
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         VisionManager.init(this, getString(R.string.mapbox_access_token))
     }
