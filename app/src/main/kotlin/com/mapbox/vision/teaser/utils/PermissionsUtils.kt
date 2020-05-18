b5c9a30fef545f47f13cde27d1878da3fbe11a04
Yury Bychyk <yury.bychyk@mapbox.com>
2020-05-18 13:45:10 +0300

    Use MainActivity in PermissionUtils

--- app/src/main/kotlin/com/mapbox/vision/teaser/utils/PermissionsUtils.kt
+++ app/src/main/kotlin/com/mapbox/vision/teaser/utils/PermissionsUtils.kt
@@ -4,13 +4,14 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.content.ContextCompat
+import com.mapbox.vision.teaser.MainActivity
 
 object PermissionsUtils {
 
     private const val PERMISSION_FOREGROUND_SERVICE = "android.permission.FOREGROUND_SERVICE"
     private const val PERMISSIONS_REQUEST_CODE = 123
 
-    fun requestPermissionsIfNotGranted(activity: Activity): Boolean {
+    fun requestPermissionsIfNotGranted(activity: MainActivity): Boolean {
         if (!allPermissionsGranted(activity) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity.requestPermissions(getRequiredPermissions(activity), PERMISSIONS_REQUEST_CODE)
             return true
@@ -18,7 +19,7 @@ object PermissionsUtils {
         return false
     }
 
-    private fun allPermissionsGranted(activity: Activity): Boolean {
+    private fun allPermissionsGranted(activity: MainActivity): Boolean {
         for (permission in getRequiredPermissions(activity)) {
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 // PERMISSION_FOREGROUND_SERVICE was added for targetSdkVersion >= 28, it is normal and always granted, but should be added to the Manifest file
@@ -33,7 +34,7 @@ object PermissionsUtils {
         return true
     }
 
-    private fun getRequiredPermissions(activity: Activity): Array<String> {
+    private fun getRequiredPermissions(activity: MainActivity): Array<String> {
         return try {
             val info = activity.packageManager?.getPackageInfo(activity.packageName, PackageManager.GET_PERMISSIONS)
             val permissions = info?.requestedPermissions
@@ -47,7 +48,7 @@ object PermissionsUtils {
         }
     }
 
-    fun allPermissionsGrantedByRequest(activity: Activity, requestCode: Int)
+    fun allPermissionsGrantedByRequest(activity: MainActivity, requestCode: Int)
             = allPermissionsGranted(activity) && requestCode == PERMISSIONS_REQUEST_CODE
 
 }
